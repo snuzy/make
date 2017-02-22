59a6562487a7bec76def5b38a0ab260321c8a21d
sixFingers <ignazio.setti@gmail.com>
2017-02-22 18:17:31 +0100

    Add Intern support and base functional test
    Update tests

---
+++ tests/functional/base.js
@@ -0,0 +1,227 @@
+define([
+	'intern',
+	'intern!tdd',
+  '../support/builder'
+], function (intern, tdd, Builder) {
+
+	// WordPress home url, without trailing slash
+	var baseUrl = intern.args.url;
+	var wpUser = intern.args.user;
+	var wpPass = intern.args.pass;
+
+	// The Builder testing interface
+	var builder;
+
+	tdd.suite('One Section Per Type Layout', function () {
+		tdd.before(function() {
+			builder = new Builder(this.remote);
+			this.remote.setWindowSize(1440, 768);
+
+			return this.remote
+				// Login
+				.get(baseUrl + '/wp-admin/post-new.php?post_type=page')
+				.findDisplayedByXpath('id("user_login")')
+					.moveMouseTo()
+					.click()
+					.type(wpUser)
+					.end()
+				.findDisplayedByXpath('id("user_pass")')
+					.moveMouseTo()
+					.click()
+					.type(wpPass)
+					.submit();
+		});
+
+		tdd.test('Set Post title', function () {
+			var command = builder.showCursor();
+			command = builder.setPostTitle('Mixed Layout Test', command);
+			command = command.sleep(1000);
+
+			return command;
+		});
+
+		tdd.test('Create Columns section', function () {
+			var command = builder.showCursor();
+
+			// Create the section
+			command = builder.createSection('columns', command);
+			command = command.sleep(1000);
+
+			command = builder.openOverlay(command);
+			command = builder.setOverlayTitle('Columns Section Title', command);
+			command = builder.applyOverlay(command);
+			command = command.sleep(1000);
+
+			command = builder.openItemDropdown(1, command);
+			command = builder.openItemOverlay(1, command);
+			command = builder.setOverlayTitle('First Column Title', command);
+			command = builder.applyOverlay(command);
+			command = command.sleep(1000);
+
+			command = builder.openItemDropdown(1, command);
+			command = builder.openItemEditor(1, command);
+			command = builder.setContent('First column content', command);
+			command = builder.applyOverlay(command);
+			command = command.sleep(1000);
+
+			command = builder.openItemDropdown(2, command);
+			command = builder.openItemOverlay(2, command);
+			command = builder.setOverlayTitle('Second Column Title', command);
+			command = builder.applyOverlay(command);
+			command = command.sleep(1000);
+
+			command = builder.openItemDropdown(2, command);
+			command = builder.openItemEditor(2, command);
+			command = builder.setContent('Second column content', command);
+			command = builder.applyOverlay(command);
+			command = command.sleep(1000);
+
+			command = builder.openItemDropdown(3, command);
+			command = builder.openItemOverlay(3, command);
+			command = builder.setOverlayTitle('Third Column Title', command);
+			command = builder.applyOverlay(command);
+			command = command.sleep(1000);
+
+			command = builder.openItemDropdown(3, command);
+			command = builder.openItemEditor(3, command);
+			command = builder.setContent('Third column content', command);
+			command = builder.applyOverlay(command);
+			command = command.sleep(1000);
+
+			command = builder.moveColumn(1, 1, command);
+			command = command.sleep(1000);
+
+			command = builder.moveColumn(3, -1, command);
+			command = command.sleep(1000);
+
+			command = builder.resizeColumn(1, 1, command);
+			command = command.sleep(1000);
+
+			return command;
+		});
+
+		tdd.test('Create Banner section', function () {
+			var command = builder.showCursor();
+			command = builder.createSection('banner', command);
+			command = command.sleep(1000);
+
+			command = builder.openOverlay(command);
+			command = builder.setOverlayTitle('Banner Section Title', command);
+			command = command.sleep(1000);
+
+			command = builder.applyOverlay(command);
+			command = command.sleep(1000);
+
+			command = builder.openItemDropdown(1, command);
+			command = builder.openItemEditor(1, command);
+			command = builder.setContent('First slide content', command);
+			command = builder.applyOverlay(command);
+			command = command.sleep(1000);
+
+			command = builder.addBannerSlide(command);
+			command = command.sleep(1000);
+
+			command = builder.openItemDropdown(2, command);
+			command = builder.openItemEditor(2, command);
+			command = builder.setContent('Second slide content', command);
+			command = builder.applyOverlay(command);
+			command = command.sleep(1000);
+
+			command = builder.addBannerSlide(command);
+			command = command.sleep(1000);
+
+			command = builder.openItemDropdown(3, command);
+			command = builder.openItemEditor(3, command);
+			command = builder.setContent('Third slide content', command);
+			command = builder.applyOverlay(command);
+
+			return command;
+		});
+
+		tdd.test('Create Gallery section', function () {
+			var command = builder.createSection('gallery');
+			command = command.sleep(1000);
+
+			command = builder.openOverlay(command);
+			command = builder.setOverlayTitle('Gallery Section Title', command);
+			command = command.sleep(1000);
+
+			command = builder.applyOverlay(command);
+
+			return command;
+		});
+
+		tdd.test('Create Panels section', function () {
+			var command = builder.createSection('panels');
+			command = command.sleep(1000);
+
+			command = builder.openOverlay(command);
+			command = builder.setOverlayTitle('Panels Section Title', command);
+			command = builder.applyOverlay(command);
+			command = command.sleep(1000);
+
+			command = builder.openItemDropdown(1, command);
+			command = builder.openItemOverlay(1, command);
+			command = builder.setOverlayTitle('First Item Title', command);
+			command = builder.applyOverlay(command);
+			command = command.sleep(1000);
+
+			command = builder.openItemDropdown(1, command);
+			command = builder.openItemEditor(1, command);
+			command = builder.setContent('First item content', command);
+			command = builder.applyOverlay(command);
+			command = command.sleep(1000);
+
+			command = builder.addPanelsItem(command);
+			command = command.sleep(1000);
+
+			command = builder.openItemDropdown(2, command);
+			command = builder.openItemOverlay(2, command);
+			command = builder.setOverlayTitle('Second Item Title', command);
+			command = builder.applyOverlay(command);
+			command = command.sleep(1000);
+
+			command = builder.openItemDropdown(2, command);
+			command = builder.openItemEditor(2, command);
+			command = builder.setContent('Second item content', command);
+			command = builder.applyOverlay(command);
+			command = command.sleep(1000);
+
+			command = builder.addPanelsItem(command);
+			command = command.sleep(1000);
+
+			command = builder.openItemDropdown(3, command);
+			command = builder.openItemOverlay(3, command);
+			command = builder.setOverlayTitle('Third Item Title', command);
+			command = builder.applyOverlay(command);
+			command = command.sleep(1000);
+
+			command = builder.openItemDropdown(3, command);
+			command = builder.openItemEditor(3, command);
+			command = builder.setContent('Third item content', command);
+			command = builder.applyOverlay(command);
+
+			return command;
+		});
+
+		tdd.test('Create Posts List section', function () {
+			var command = builder.createSection('postlist');
+			command = command.sleep(1000);
+
+			command = builder.openOverlay(command);
+			command = builder.setOverlayTitle('Posts List Section Title', command);
+			command = command.sleep(1000);
+
+			command = builder.applyOverlay(command);
+
+			return command;
+		});
+
+		tdd.test('Publish page', function () {
+			return this.remote
+				// Publish page
+				.findByXpath('//input[@id="publish"][1]')
+					.submit();
+		});
+	});
+});
---
+++ tests/support/builder.js
@@ -0,0 +1,236 @@
+define(function (require) {
+		// The overlay which is currently open
+	var overlaySelector = '(//div[@class="ttfmake-overlay ttfmake-configuration-overlay"])[last()]';
+
+	function Builder(remote) {
+		this.remote = remote;
+  }
+
+  Builder.prototype = {
+  	showCursor: function(command) {
+  		command = command || this.remote;
+  		return command
+  			.execute(this.buildCursor)
+  			.end();
+  	},
+
+  	buildCursor: function() {
+  		var cursorRadius = 10;
+  		var el = document.createElement('div');
+  		el.style.pointerEvents = 'none';
+  		el.style.position = 'fixed';
+  		el.style.zIndex = '99999999999';
+  		el.style.top = '0';
+  		el.style.left = '0';
+  		el.style.display = 'block';
+  		el.style.width = (cursorRadius * 2) + 'px';
+  		el.style.height = (cursorRadius * 2) + 'px';
+  		el.style.backgroundColor = '#666';
+  		el.style.borderRadius = '50%';
+  		el.style.border = '2px solid #666';
+
+  		document.addEventListener('mousemove', function(e) {
+  			el.style.top = (e.clientY - cursorRadius) + 'px';
+  			el.style.left = (e.clientX - cursorRadius) + 'px';
+  		});
+
+  		document.addEventListener('mousedown', function(e) {
+  			el.style.backgroundColor = '#fff';
+  		});
+
+  		document.addEventListener('mouseup', function(e) {
+  			setTimeout(function() {
+  				el.style.backgroundColor = '#666';
+  			}, 250);
+  		});
+
+  		document.body.appendChild(el);
+  	},
+
+  	// Return the slug (or type,
+  	// but with exceptions) of a section
+  	getSectionSlug: function(type) {
+  		return 'columns' !== type && type || 'text';
+  	},
+
+  	// Sets the Post title.
+  	setPostTitle: function(title, command) {
+  		command = command || this.remote;
+  		return command
+  			.findByCssSelector('#title')
+  				.moveMouseTo()
+					.click()
+					.type(title)
+					.end();
+  	},
+
+  	// Clicks a Create Section button.
+  	createSection: function(type, command) {
+  		var slug = this.getSectionSlug(type);
+
+  		command = command || this.remote;
+  		return command
+				// Create section
+				.findByCssSelector('#ttfmake-menu-list-item-link-' + slug + ' span')
+					.moveMouseTo()
+					.click()
+					.end();
+  	},
+
+  	// Opens the configuration overlay
+  	// of the last added section.
+  	openOverlay: function(command) {
+  		command = command || this.remote;
+  		return command
+	  		// Open configuration overlay
+				.findByCssSelector('.ttfmake-section:last-child .ttfmake-section-header .ttfmake-section-configure')
+					.moveMouseTo()
+					.click()
+					.end();
+  	},
+
+  	// Clicks the conf dropdown icon of the nth(item) item.
+  	openItemDropdown: function(item, command) {
+  		command = command || this.remote;
+  		return command
+	  		// Open configuration overlay
+				.findByCssSelector('.ttfmake-section:last-child  .ttfmake-section-body .ui-sortable > div:nth-child(' + item + ') .ttfmake-configure-item-button')
+					.moveMouseTo()
+					.click()
+					.sleep(500)
+					.end();
+  	},
+
+  	// Clicks the cog icon of the nth(item) item's dropdown.
+  	openItemOverlay: function(item, command) {
+  		command = command || this.remote;
+  		return command
+	  		// Open configuration overlay
+				.findByCssSelector('.ttfmake-section:last-child  .ttfmake-section-body .ui-sortable > div:nth-child(' + item + ') .ttfmake-icon-cog')
+					.moveMouseTo()
+					.click()
+					.end();
+  	},
+
+  	// Clicks the pencil icon of the nth(item) item's dropdown.
+  	openItemEditor: function(item, command) {
+  		command = command || this.remote;
+  		return command
+	  		// Open configuration overlay
+				.findByCssSelector('.ttfmake-section:last-child  .ttfmake-section-body .ui-sortable > div:nth-child(' + item + ') .ttfmake-icon-pencil')
+					.moveMouseTo()
+					.click()
+					.end();
+  	},
+
+  	// Clicks the pencil icon of the nth(item) item's dropdown.
+  	openItemEditorPlaceholder: function(item, command) {
+  		command = command || this.remote;
+  		return command
+	  		// Open configuration overlay
+				.findByCssSelector('.ttfmake-section:last-child .ttfmake-section-body .ui-sortable > div:nth-child(' + item + ') .ttfmake-iframe-content-placeholder')
+	  			.moveMouseTo()
+					.click()
+					.sleep(500)
+					.end()
+  	},
+
+  	// Sets the title in the currently open
+  	// configuration overlay.
+  	setOverlayTitle: function(title, command) {
+  		command = command || this.remote;
+  		return command
+  			// Set a title for the section.
+  			// CSS selection is picky with overlays,
+  			// using Xpath for that reason.
+				.findByXpath(overlaySelector + '/div[1]/div[1]/div[2]/div[1]/input[1]')
+					.moveMouseTo()
+					.click()
+					.type(title)
+					.end();
+  	},
+
+  	// Sets the content in the currently open editor overlay
+  	setContent: function(content, command) {
+  		command = command || this.remote;
+  		return command
+  			.findByCssSelector('#ttfmake-tinymce-overlay iframe')
+					.moveMouseTo(20, 20)
+					.clickMouseButton()
+					.type(content)
+					.end();
+  	},
+
+  	// Drags the 1-indexed column element towards the [1|-1] direction.
+  	moveColumn: function(column, direction, command) {
+  		var dragSteps = 10;
+
+  		command = command || this.remote;
+  		command = command
+  			.findByCssSelector('.ttfmake-section:last-child .ttfmake-section-body .ui-sortable .ttfmake-text-column:nth-child(' + column + ')')
+	  			.moveMouseTo(0, 0)
+	  			.pressMouseButton(0)
+	  			.sleep(1000);
+
+	  	for (var s = 0; s < dragSteps; s++) {
+	  		command = command
+	  			.moveMouseTo(direction * s * 10, 1)
+	  			.sleep(200);
+	  	}
+
+  		return command
+	  			.sleep(1000)
+	  			.releaseMouseButton(0)
+	  			.end();
+  	},
+
+  	// Resizes the 1-based column in the given direction.
+  	resizeColumn: function(column, direction, command) {
+  		var dragSteps = 10;
+
+  		command = command || this.remote;
+  		return command
+  			.findByCssSelector('.ttfmake-section:last-child .ttfmake-section-body .ui-sortable > div:nth-child(' + column + ')')
+	  			.moveMouseTo(direction > 0 && 300 || -10, 0)
+	  			.pressMouseButton(0)
+	  			.sleep(1000)
+	  			.moveMouseTo(direction * 100, 0)
+	  			.sleep(1000)
+	  			.releaseMouseButton(0)
+	  			.end();
+  	},
+
+  	addBannerSlide: function(command) {
+  		command = command || this.remote;
+  		return command
+  			.findByCssSelector('.ttfmake-section:last-child .ttfmake-banner-add-item-link')
+	  			.moveMouseTo()
+					.click()
+					.end();
+  	},
+
+  	addPanelsItem: function(command) {
+  		command = command || this.remote;
+  		return command
+  			.findByCssSelector('.ttfmake-section:last-child .ttfmp-panels-add-item-link')
+	  			.moveMouseTo()
+					.click()
+					.end();
+  	},
+
+  	// Clicks the update changes button of the current overlay.
+  	applyOverlay: function(command) {
+  		command = command || this.remote;
+  		return command
+  			// Click update changes.
+  			// CSS selection is picky with overlays,
+  			// using Xpath for that reason.
+				.findByXpath(overlaySelector + '/div[1]/div[1]/div[3]/span[1]')
+					.moveMouseTo()
+					.click()
+					.end();
+  	}
+  };
+
+  return Builder;
+});
