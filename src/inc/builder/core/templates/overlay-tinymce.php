b1b420adb9b74c812af086b64534626da62d040f
sixFingers <ignazio.setti@gmail.com>
2017-03-21 10:56:47 +0100

    Fix height of content editor

--- src/inc/builder/core/templates/overlay-tinymce.php
+++ src/inc/builder/core/templates/overlay-tinymce.php
@@ -10,7 +10,7 @@ wp_editor( '', 'make', array(
 		'wp_autoresize_on' => false,
 		'resize'           => false,
 	),
-	'editor_height' => 320
+	'editor_height' => 270
 ) );
 
 get_template_part( '/inc/builder/core/templates/overlay', 'footer' );
