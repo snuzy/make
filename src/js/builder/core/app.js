3bc7de3015327270a2c0375d01e75524c614b0e0
sixFingers <ignazio.setti@gmail.com>
2017-04-21 13:55:41 +0200

    Move ttfmake-has-image-set to the whole overlay

--- src/js/builder/core/app.js
+++ src/js/builder/core/app.js
@@ -343,7 +343,9 @@ var oneApp = oneApp || {}, ttfMakeFrames = ttfMakeFrames || [];
 		onUploaderFrameRemoveImage: function() {
 			// Remove the image
 			this.$currentPlaceholder.css('background-image', '');
-			this.$currentPlaceholder.parent().removeClass('ttfmake-has-image-set');
+			this.$currentPlaceholder
+				.parents('.ttfmake-configuration-overlay')
+				.removeClass('ttfmake-has-image-set');
 
 			// Trigger event on the uploader to propagate it to calling view
 			this.$currentPlaceholder.trigger('mediaRemoved')
@@ -360,7 +362,9 @@ var oneApp = oneApp || {}, ttfMakeFrames = ttfMakeFrames || [];
 			var props = wp.media.string.props({}, attachment);
 			// Show the image
 			this.$currentPlaceholder.css('background-image', 'url(' + attachment.url + ')');
-			this.$currentPlaceholder.parent().addClass('ttfmake-has-image-set');
+			this.$currentPlaceholder
+				.parents('.ttfmake-configuration-overlay')
+				.addClass('ttfmake-has-image-set');
 			// Trigger events on the view
 			this.$currentPlaceholder.trigger('mediaSelected', attachment);
 		},
