d27791b88e3685034c47737dc2b3585e62dedb88
sixFingers <ignazio.setti@gmail.com>
2017-05-05 10:54:42 +0200

    Cleanup, faster sliding

--- src/js/builder/core/views/overlay.js
+++ src/js/builder/core/views/overlay.js
@@ -336,17 +336,15 @@ var oneApp = oneApp || {};
 			var $dividers = $('.ttfmake-configuration-divider-wrap', this.$el).not($divider);
 			var $body = $('.ttfmake-overlay-body', this.$el);
 
-			// $dividers.removeClass('open-wrap');
-			// $divider.addClass('open-wrap');
-
 			$dividers.each(function() {
 				var $this = $(this);
-				$this.next().slideUp(400, function() {
+				$this.next().slideUp(200, function() {
 					$this.removeClass('open-wrap');
 				});
 			});
 
 			$divider.next().slideDown({
+				duration: 200,
 				step: function() {
 					var offset = $divider.position().top + $body.scrollTop() - $divider.outerHeight();
 					$body.scrollTop(offset);
