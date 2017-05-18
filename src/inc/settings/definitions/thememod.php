5fc04c4907ed13db36da31e8ca7e7b80761692fa
Tomas Stark <tom@tomstark.me>
2017-05-18 16:38:58 +0200

    Panel title color defaults

--- src/inc/settings/definitions/thememod.php
+++ src/inc/settings/definitions/thememod.php
@@ -226,7 +226,9 @@ $this->add_settings(
 		'footer-border-color'               => array( 'default' => '#b9bcbf' ),
 		'footer-background-color'           => array( 'default' => '#eaecee' ),
 		'color-panel'						=> array( 'default' => '#eaecee' ),
+		'color-panel-title'						=> array( 'default' => '#b9bcbf' ),
 		'color-panel-active'				=> array( 'default' => '#3070d1' ),
+		'color-panel-active-title'				=> array( 'default' => '#eaecee' ),
 	),
 	array(
 		'sanitize' => 'maybe_hash_hex_color',
