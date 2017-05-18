79c3c5dd705633e590a549704aac35da8dd0b1c1
Tomas Stark <tom@tomstark.me>
2017-05-18 15:22:10 +0200

    Panels Customizer color for Panels - add defaults

--- src/inc/settings/definitions/thememod.php
+++ src/inc/settings/definitions/thememod.php
@@ -225,6 +225,8 @@ $this->add_settings(
 		'footer-link-hover-color'           => array( 'default' => '' ),
 		'footer-border-color'               => array( 'default' => '#b9bcbf' ),
 		'footer-background-color'           => array( 'default' => '#eaecee' ),
+		'color-panel'						=> array( 'default' => '#eaecee' ),
+		'color-panel-active'				=> array( 'default' => '#3070d1' ),
 	),
 	array(
 		'sanitize' => 'maybe_hash_hex_color',
