7cb5294b22a216f574946f5d629ed81f9d2c9acf
sixFingers <ignazio.setti@gmail.com>
2017-06-29 18:09:21 +0200

    Refactor section builder template partials

--- src/inc/builder/core/templates/section-footer.php
+++ src/inc/builder/core/templates/section-footer.php
@@ -2,14 +2,7 @@
 /**
  * @package Make
  */
-
-global $ttfmake_section_data, $ttfmake_overlay_id;
-
-$ttfmake_overlay_id = 'ttfmake-overlay-{{ id }}';
 ?>
 
-	<?php get_template_part( '/inc/builder/core/templates/overlay', 'configuration' ); ?>
-
-    <textarea name="ttfmake-section-json[{{ id }}]" style="display: none;">{{ JSON.stringify(toJSON()) }}</textarea>
-
+	<textarea class="ttfmake-section-json" name="ttfmake-section-json[{{ data.get('id') }}]" style="display: none;">{{ JSON.stringify( data.toJSON() ) }}</textarea>
 </div>
--- src/inc/builder/core/templates/section-header.php
+++ src/inc/builder/core/templates/section-header.php
@@ -3,8 +3,6 @@
  * @package Make
  */
 
-global $ttfmake_section_data, $ttfmake_is_js_template;
-
 $links = array(
 	100 => array(
 		'href'  => '#',
@@ -14,13 +12,12 @@ $links = array(
 	)
 );
 
-if ( ! empty( $ttfmake_section_data['section']['config'] ) ) {
+if ( ! empty( $ttfmake_section_data['config'] ) ) {
 	$links[25] = array(
 		'href'  => '#',
 		'class' => 'ttfmake-section-configure ttfmake-overlay-open',
 		'label' => __( 'Configure section', 'make' ),
 		'title' => __( 'Configure section', 'make' ),
-		'other' => 'data-overlay="#ttfmake-overlay-{{ id }}"'
 	);
 }
 
@@ -46,7 +43,7 @@ $links = apply_filters( 'make_builder_section_links', $links );
 ksort( $links );
 ?>
 
-<div class="ttfmake-section{{ get('state') == 'open' && ' ttfmake-section-open' || ''}} ttfmake-section-{{ get('section-type') }}" id="ttfmake-section-{{ get('id') }}" data-id="{{ get('id') }}" data-section-type="{{ get('section-type') }}">
+<div class="ttfmake-section{{ 'closed' === data.get('state') ? '' : ' ttfmake-section-open' }} ttfmake-section-{{ data.get('section-type') }}" id="ttfmake-section-{{ data.get('id') }}" data-id="{{ data.get('id') }}" data-section-type="{{ data.get('section-type') }}">
 	<?php
 	/**
 	 * Execute code before the section header is displayed.
@@ -56,10 +53,10 @@ ksort( $links );
 	do_action( 'make_before_section_header' );
 	?>
 	<div class="ttfmake-section-header">
-		<h3{{ (get('title')) ? ' class=has-title' : '' }}>
-			<span class="ttfmake-section-header-title">{{ get('title') }}</span><em><?php echo ( esc_html( $ttfmake_section_data['section']['label'] ) ); ?></em>
-			
-			<span class="ttfmake-section-draft-indicator" style="display: {{ (parseInt(get('draft'), 10) === 1) ? 'inline-block' : 'none' }}">
+		<h3{{ (data.get('title')) ? ' class=has-title' : '' }}>
+			<span class="ttfmake-section-header-title">{{ data.get('title') }}</span><em><?php echo ( esc_html( $ttfmake_section_data['label'] ) ); ?></em>
+
+			<span class="ttfmake-section-draft-indicator" style="display: {{ (parseInt(data.get('draft'), 10) === 1) ? 'inline-block' : 'none' }}">
 				<span class="ttfmake-section-draft-indicator-inner">Draft</span>
 			</span>
 		</h3>
@@ -70,13 +67,12 @@ ksort( $links );
 				$id    = ( isset( $link['id'] ) ) ? ' id="' . esc_attr( $link['id'] ) . '"' : '';
 				$label = ( isset( $link['label'] ) ) ? esc_html( $link['label'] ) : '';
 				$title = ( isset( $link['title'] ) ) ? ' title="' . esc_html( $link['title'] ) . '"' : '';
-				$other = ( isset( $link['other'] ) ) ? ' ' . $link['other'] : '';
 
 				// Set up the class value with a base class
 				$class_base = ' class="ttfmake-builder-section-link';
 				$class      = ( isset( $link['class'] ) ) ? $class_base . ' ' . esc_attr( $link['class'] ) . '"' : '"';
 				?>
-				<a<?php echo $href . $id . $class . $title . $other; ?>>
+				<a<?php echo $href . $id . $class . $title; ?>>
 					<span>
 						<?php echo $label; ?>
 					</span>
@@ -91,4 +87,3 @@ ksort( $links );
 	</div>
 	<div class="clear"></div>
 	<div class="ttfmake-section-body">
-		<input type="hidden" value="<?php echo $ttfmake_section_data['section']['id']; ?>" name="<?php echo ttfmake_get_section_name( $ttfmake_section_data, true ); ?>[section-type]" />
