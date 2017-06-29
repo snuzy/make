321fef277596393471dd9b89af2050b5602be658
sixFingers <ignazio.setti@gmail.com>
2017-06-29 18:08:59 +0200

    Refactor overlay templates

--- src/inc/builder/core/templates/overlay-footer.php
+++ src/inc/builder/core/templates/overlay-footer.php
@@ -1,6 +1,6 @@
 			</div>
 			<div class="ttfmake-overlay-footer">
-				<span class="ttfmake-overlay-close-update button button-primary button-large" aria-hidden="true"><?php esc_html_e( 'Update changes', 'make' ); ?></span>
+				<span class="ttfmake-overlay-close-update button button-primary button-large" aria-hidden="true"><?php _e( 'Update changes', 'make' ); ?></span>
 			</div>
 		</div>
 	</div>
