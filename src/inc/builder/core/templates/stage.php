09332b6820d651c67fa6ad8689c32addd706d8e3
sixFingers <ignazio.setti@gmail.com>
2017-06-30 14:52:13 +0200

    Refactor section stage template to ensure the stage has no text/empty nodes

--- src/inc/builder/core/templates/stage.php
+++ src/inc/builder/core/templates/stage.php
@@ -4,23 +4,24 @@
  */
 
 global $ttfmake_sections;
+
+/**
+ * Execute code before the builder stage is displayed.
+ *
+ * @since 1.2.3.
+ */
+do_action( 'make_before_builder_stage' );
 ?>
 
-<div class="ttfmake-stage ttfmake-stage-closed" id="ttfmake-stage">
-	<?php
-	/**
-	 * Execute code before the builder stage is displayed.
-	 *
-	 * @since 1.2.3.
-	 */
-	do_action( 'make_before_builder_stage' );
+<div class="ttfmake-stage ttfmake-stage-closed" id="ttfmake-stage"></div>
 
-	/**
-	 * Execute code after the builder stage is displayed.
-	 *
-	 * @since 1.2.3.
-	 */
-	do_action( 'make_after_builder_stage' );
+<textarea name="ttfmake-sections-json" id="ttfmake-sections-json" style="display: none;"></textarea>
+
+<?php
+/**
+ * Execute code after the builder stage is displayed.
+ *
+ * @since 1.2.3.
+ */
+do_action( 'make_after_builder_stage' );
 ?>
-	<textarea name="ttfmake-sections-json" id="ttfmake-sections-json" style="display: none;"></textarea>
-</div>
