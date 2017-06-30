95c7c2ad682954517cc218cefb54bd6123cbc262
sixFingers <ignazio.setti@gmail.com>
2017-06-30 18:14:09 +0200

    Output overlay titles

--- src/inc/builder/core/templates/overlay-header.php
+++ src/inc/builder/core/templates/overlay-header.php
@@ -3,7 +3,7 @@
 		<div class="ttfmake-overlay-dialog">
 			<div class="ttfmake-overlay-header">
 				<div class="ttfmake-overlay-window-head">
-					<div class="ttfmake-overlay-title"><?php if ( ! empty( $ttfmake_overlay_title ) ) : echo $ttfmake_overlay_title; else : _e( 'Configuration', 'make' ); endif; ?></div>
+					<div class="ttfmake-overlay-title"><?php echo $ttfmake_overlay_title; ?></div>
 					<button type="button" class="media-modal-close ttfmake-overlay-close-discard">
 						<span class="media-modal-icon">
 					</button>
