c6c7f77a9e4ea83378f7efdb8b6c65c0568517d7
sixFingers <ignazio.setti@gmail.com>
2017-08-16 12:41:42 +0200

    Deactivate unsupported Make Plus

--- src/inc/activation.php
+++ src/inc/activation.php
@@ -42,3 +42,45 @@ function make_deactivation_notice() {
 		$message
 	);
 }
+
+/**
+ * Deactivate Make Plus if it doesn't meet
+ * the minimum version requirements.
+ *
+ * @since 1.9.1.
+ *
+ * @return void
+ */
+if ( defined( 'MAKEPLUS_VERSION' ) && version_compare( MAKEPLUS_VERSION, '1.9.0' ) < 0 ) {
+	$plugin_name = plugin_basename( makeplus_get_plugin_directory() );
+	$current_url = $_SERVER['HTTP_REFERER'];
+	$redirect_url = add_query_arg( 'make-plus-unsupported', '1', $current_url );
+
+	// Deactivate and redirect back with error
+	deactivate_plugins( "{$plugin_name}/{$plugin_name}.php" );
+	wp_safe_redirect( $redirect_url );
+	exit();
+}
+
+/**
+ * Show unsupported Make Plus version notice.
+ *
+ * @since 1.9.1.
+ *
+ * @return void
+ */
+function makeplus_unsupported_version() {
+	$message = sprintf(
+		esc_html__( 'Make %1$s isn\'t compatible with your version of Make Plus. To avoid issues, Make Plus has been deactivated. Please update Make Plus.', 'make' ),
+		esc_html( TTFMAKE_VERSION )
+	);
+
+	printf(
+		'<div class="notice notice-error error"><p>%s</p></div>',
+		$message
+	);
+}
+
+if( isset( $_GET['make-plus-unsupported'] ) ) {
+	add_action( 'admin_notices', 'makeplus_unsupported_version' );
+}
