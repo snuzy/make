4d8649ea4eb2cc698b08331b2510cb7979da105f
sixFingers <ignazio.setti@gmail.com>
2017-09-28 13:45:01 +0200

    Scope shared settings and upsells to admin page

--- src/inc/sections/setup.php
+++ src/inc/sections/setup.php
@@ -71,15 +71,17 @@ class MAKE_Sections_Setup extends MAKE_Util_Modules implements MAKE_Sections_Set
 		MAKE_Sections_Banner_Definition::register();
 		MAKE_Sections_Gallery_Definition::register();
 
-		add_filter( 'make_section_settings', array( $this, 'full_width_settings' ), 20, 2 );
-		add_filter( 'make_sections_defaults', array( $this, 'full_width_defaults' ), 20 );
-		add_filter( 'make_prepare_data_section', array( $this, 'full_width_save_data' ), 20, 2 );
+		if ( is_admin() ) {
+			add_filter( 'make_section_settings', array( $this, 'full_width_settings' ), 30, 2 );
+			add_filter( 'make_sections_defaults', array( $this, 'full_width_defaults' ) );
+			add_filter( 'make_prepare_data_section', array( $this, 'full_width_save_data' ), 20, 2 );
 
-		// Upsells
-		if ( ! Make()->plus()->is_plus() ) {
-			add_filter( 'make_section_settings', array( $this, 'master_demo_setting' ), 40, 2 );
-			add_filter( 'make_section_settings', array( $this, 'section_draft_demo_setting' ), 50, 2 );
-			add_filter( 'make_section_settings', array( $this, 'section_code_demo_setting' ), 60, 2 );
+			// Upsells
+			if ( ! Make()->plus()->is_plus() ) {
+				add_filter( 'make_section_settings', array( $this, 'master_demo_setting' ), 40, 2 );
+				add_filter( 'make_section_settings', array( $this, 'section_draft_demo_setting' ), 50, 2 );
+				add_filter( 'make_section_settings', array( $this, 'section_code_demo_setting' ), 60, 2 );
+			}
 		}
 	}
 
