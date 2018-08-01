63796af83198678b19cd628695e815be44765ca2
sixFingers <ignazio.setti@gmail.com>
2018-08-01 16:36:52 +0200

    Bump version

--- src/functions.php
+++ src/functions.php
@@ -6,7 +6,7 @@
 /**
  * The current version of the theme.
  */
-define( 'TTFMAKE_VERSION', '1.9.12' );
+define( 'TTFMAKE_VERSION', '1.9.13' );
 
 /**
  * The minimum version of WordPress required for Make.
--- src/inc/font/source/google-data.php
+++ src/inc/font/source/google-data.php
@@ -3,7 +3,7 @@
  * @package Make
  */
 
-// Updated: 2018-07-03T10:23:26+00:00
+// Updated: 2018-08-01T14:36:40+00:00
 
 // Bail if this isn't being included inside of MAKE_Font_Source_Google.
 if ( ! isset( $this ) || ! $this instanceof MAKE_Font_Source_Google ) {
