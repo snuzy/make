52f4c3e510eb69e0f9a8be234b5a057a671522c5
sixFingers <ignazio.setti@gmail.com>
2018-12-05 16:46:15 +0100

    Fix timeouts

--- src/js/gutenberg.js
+++ src/js/gutenberg.js
@@ -1,6 +1,10 @@
 ( function( $, data ) {
 	var saveTimer;
 
+	var savePost = function() {
+		$( '.editor-post-publish-button' ).click();
+	}
+
 	var onPostComplete = function() {
 		var complete = data.select( 'core/editor' ).didPostSaveRequestSucceed();
 
@@ -8,14 +12,14 @@
 			clearInterval( saveTimer );
 			setTimeout( function() {
 				window.location.reload();
-			}, 5000 );
+			}, 2000 );
 		}
 	}
 
 	$( document ).ready( function() {
 		$( 'body' ).on( 'change', '.editor-page-attributes__template', function( e ) {
 			if ( 'template-builder.php' === $( e.target ).val() ) {
-				data.dispatch( 'core/editor' ).savePost();
+				setTimeout( savePost, 500 );
 				saveTimer = setInterval( onPostComplete, 100 );
 			}
 		} );
