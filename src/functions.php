ba0c3d19eda92b2704efbe2bbde0f6c63111a907
sixFingers <ignazio.setti@gmail.com>
2018-12-06 10:00:50 +0100

    Bump version

--- src/functions.php
+++ src/functions.php
@@ -6,7 +6,7 @@
 /**
  * The current version of the theme.
  */
-define( 'TTFMAKE_VERSION', '1.9.15' );
+define( 'TTFMAKE_VERSION', '1.9.16' );
 
 /**
  * The minimum version of WordPress required for Make.
