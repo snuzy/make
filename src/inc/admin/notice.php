078c676f687f1041a3f8ad9d6f8109c8e228054b
sixFingers <ignazio.setti@gmail.com>
2018-12-06 13:37:26 +0100

    Allow linebreak

--- src/inc/admin/notice.php
+++ src/inc/admin/notice.php
@@ -523,6 +523,7 @@ final class MAKE_Admin_Notice implements MAKE_Admin_NoticeInterface, MAKE_Util_H
 	private function sanitize_message( $message ) {
 		$allowedtags = wp_kses_allowed_html();
 		$allowedtags['a']['target'] = true;
+		$allowedtags['br'] = true;
 		return wp_kses( $message, $allowedtags );
 	}
 }
