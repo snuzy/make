0f314dca15a7491eca1e67d30ac4e680840a7a51
sixFingers <ignazio.setti@gmail.com>
2018-12-12 08:58:09 +0100

    Bump version

--- src/inc/font/source/google-data.php
+++ src/inc/font/source/google-data.php
@@ -3,7 +3,7 @@
  * @package Make
  */
 
-// Updated: 2018-12-06T12:47:13+00:00
+// Updated: 2018-12-12T07:57:47+00:00
 
 // Bail if this isn't being included inside of MAKE_Font_Source_Google.
 if ( ! isset( $this ) || ! $this instanceof MAKE_Font_Source_Google ) {
